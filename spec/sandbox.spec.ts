abf31e539361d9430924145c0e834795f7b9ad9e
Girma Nigusse <xgirma@gmail.com>
2020-02-17 00:59:09 -0500

    cleanup

--- spec/sandbox.spec.ts
+++ spec/sandbox.spec.ts
@@ -1,20 +1,18 @@
-import { strictEqual } from "assert";
+import assert from "assert";
 import { before } from "mocha";
 import { browser, by, element } from "protractor";
-
-const url: string = "https://e2e-boilerplates.github.io/sandbox/";
 
 describe("Sandbox", () => {
   before(function fn() {
     this.timeout(20000);
-    browser.get(url);
+    browser.get("https://e2e-boilerplates.github.io/sandbox/");
   });
 
   it("should be on Sandbox", async () => {
     const title = await browser.getTitle();
     const header = element(by.css("h1"));
 
-    strictEqual(title, "Sandbox");
-    strictEqual(await header.getText(), "Sandbox");
+    assert.strictEqual(title, "Sandbox");
+    assert.strictEqual(await header.getText(), "Sandbox");
   });
 });
